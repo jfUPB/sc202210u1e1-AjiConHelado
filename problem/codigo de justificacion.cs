76222e7019abd32517f37871327a6911c8ba27fd
pablo <pablo.zulaica@upb.edu.co>
2022-02-12 00:48:55 -0500

    se agrego el codigo de alto nivel en el que se basa la wiki para explicar el problema

---
+++ problem/codigo de justificacion.cs
@@ -0,0 +1,52 @@
+
+
+
+
+list <int> input_teclado=new list<int>(24576,0);
+
+list <int> tecla_f= new list<int>(70,0);
+
+list <int> tecla_c= new list<int>(67,0);
+int color_tinte =0;
+
+while(i=o,i==1) //loopinfinito
+{
+  color_pantalla=color_tinte;
+  espacio_actual = 24575;
+
+  while(color_pantalla= -1) //tinte negro
+{
+    if(input_teclado[1] == 1 ) 
+    {
+        if(tecla_c[1]=1)
+        {
+       color_tinte = 0;
+       fill_loop(color_tinte,espacio_actual)
+        }
+    }
+      
+}
+  espacio_actual = 24575;
+
+  while(color_pantalla==0) //tinte blanco
+  if(input_teclado[1] == 1 ) 
+    {
+        if(tecla_f[1]=1)
+        {
+       color_tinte = -1;
+       fill_loop(color_tinte)
+        }
+    }
+    
+}
+void fill_loop(int color_tinte,int espacio_actual)
+{
+    
+int pantalla= new int[espacio_final.lenght];
+for(espacio_actual > 16384, espacio_actual--)
+{
+    espacio_final= espacio actual-16384;
+pantalla[espacio_final]=color_tinte;
+}
+
+}
