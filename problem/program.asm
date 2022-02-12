ef80a3f741a1af5fb5c8c8d4cf59ddaa020f9218
pablo <pablo.zulaica@upb.edu.co>
2022-02-11 22:55:45 -0500

    se creo la deteccion del input de la tecla c,en el loop del teclado se creo la condicion solo cuando la pantalla este en negro busque input de la tecla c, se enlazo el input de la tecla c con el fill loop y se cambia el tinte a -1

--- problem/program.asm
+++ problem/program.asm
@@ -11,10 +11,22 @@
 	@70
 	d=a
 	@tecla_f // se crea la variable de la tecla f
-	
+	m=d
+	@67
+	d=a
+	@tecla_c // se crea la variable de la tecla c
+	m=d
+
     
 	(loop_teclado)
 
+		 @color_tinte
+	    d=m
+	    @tinte_negro
+	    d;jne
+
+
+		(tinte_blanco) //detecto que el tinte es blanco se mete al loop de deteccion de tecla f
 			@24575
 	    d=a
 	    @pixel_actual
@@ -29,23 +41,42 @@
     	d=d-m
 	    @fill_loop
 	    d;jeq
-	    // repite el loop hasta que detecte la tecla f 
+	    // repite el loop hasta que detecte la tecla f o detecte que el tinte de la pantalla sea negro
 	    @loop_teclado
         0;jmp
 
+
+		(tinte_negro) //detecto que el tinte es negro, se metio al loop de deteccion de la tecla c
+		@24575
+	    d=a
+	    @pixel_actual
+	    m=d
+	    // se revisa si hay algun imput en el teclado y lo compara con el valor de la letra c
+	    @input_teclado
+	    a=m
+	    d=m
+	    @color_tinte
+	    m=0
+	    @tecla_c
+	    d=d-m
+	    @fill_loop
+	    d;jeq
+        @loop_teclado
+	 0;jmp
+
+
 		(fill_loop)		              
-	         @color_tinte  // se empieza el loop de el llenado de los pixeles de la pantalla si el valor de color_tinte es negro osea -1
+	         @color_tinte  // se empieza el loop de el llenado de los pixeles de la pantalla si el valor de color_tinte es 0 se pinta blanco, si es - 1 negro
 	         d=m
 	        @pixel_actual
 	         a=m   // los pixeles empiezan a cambiar su valor al color del tinte desde la posicion 24575 y se hace un loop de resta hasta la posicion 16384   
-	         m=d // que se traducen a la primera y ultima posicion de memoria en la pantalla, 
+	         m=d // que se traducen a la primera y ultima posicion de memoria en la pantalla, luego se comprueba si la resta de la posicion actual y 16384 es 0, 
+                  //si es el caso se rompe el loop y se devuelve al loop del teclado
             @pixel_actual
 	        d=m
          	@16384
-	         d=m
-         	@16384
 	        d=d-a
-	         @fin
+	         @loop_teclado
 	         d;jle
 	        
 	         @pixel_actual
@@ -53,8 +84,4 @@
 	        @fill_loop
 	     0;jmp
 
-
-          (fin)
-          @0
-          @fin
-	     0;jmp
+	
