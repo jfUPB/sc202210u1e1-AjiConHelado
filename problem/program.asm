a00062872e3cacdbfb22a8af661896ca0bd01baf
pablo <pablo.zulaica@upb.edu.co>
2022-02-11 23:11:53 -0500

    soy estupido, los jump y los registros de la m, a y d estaban en minusculas

--- problem/program.asm
+++ problem/program.asm
@@ -5,81 +5,81 @@
 // declaracion de variables
    
 	@24576  // se crea la variable del input del teclado
-	d=a
+	D=A
 	@input_teclado
-	m=d
+	M=D
 	@70
-	d=a
+	D=A
 	@tecla_f // se crea la variable de la tecla f
-	m=d
+	M=D
 	@67
-	d=a
+	D=A
 	@tecla_c // se crea la variable de la tecla c
-	m=d
+	M=D
 
     
 	(loop_teclado)
 
 		 @color_tinte
-	    d=m
+	    D=M
 	    @tinte_negro
-	    d;jne
+	    D;JNE
 
 
 		(tinte_blanco) //detecto que el tinte es blanco se mete al loop de deteccion de tecla f
 		@24575
-	    d=a
+	    D=A
 	    @pixel_actual
-	    m=d
+	    M=D
 	    // se revisa si hay algun imput en el teclado y lo compara con el valor de la letra c
 	    @input_teclado
-	    a=m
-	    d=m
+	    A=M
+	    D=M
 	    @color_tinte
-	    m=-1
+	    M=-1
 	    @tecla_f
-    	d=d-m
+    	D=D-M
 	    @fill_loop
-	    d;jeq
+	    D;JEQ
 	    // repite el loop hasta que detecte la tecla f o detecte que el tinte de la pantalla sea negro
 	    @loop_teclado
-        0;jmp
+        0;JMP
 
 
 		(tinte_negro) //detecto que el tinte es negro, se metio al loop de deteccion de la tecla c
 		@24575
-	    d=a
+	    D=A
 	    @pixel_actual
-	    m=d
+	    M=D
 	    // se revisa si hay algun imput en el teclado y lo compara con el valor de la letra c
 	    @input_teclado
-	    a=m
-	    d=m
+	    A=M
+	    D=M
 	    @color_tinte
-	    m=0
+	    M=0
 	    @tecla_c
-	    d=d-m
+	    D=D-M
 	    @fill_loop
-	    d;jeq
+	    D;JEQ
         @loop_teclado
-	 0;jmp
+	 0;JMP
 
 
 		(fill_loop)		              
 	         @color_tinte  // se empieza el loop de el llenado de los pixeles de la pantalla si el valor de color_tinte es 0 se pinta blanco, si es - 1 negro
-	         d=m
+	         D=M
 	        @pixel_actual
-	         a=m   // los pixeles empiezan a cambiar su valor al color del tinte desde la posicion 24575 y se hace un loop de resta hasta la posicion 16384   
-	         m=d // que se traducen a la primera y ultima posicion de memoria en la pantalla, luego se comprueba si la resta de la posicion actual y 16384 es 0, 
+	         A=M   // los pixeles empiezan A cambiar su valor al color del tinte desde la posicion 24575 y se hace un loop de resta hasta la posicion 16384   
+	         M=D // que se traducen A la primera y ultima posicion de memoria en la pantalla, luego se comprueba si la resta de la posicion actual y 16384 es 0, 
                   //si es el caso se rompe el loop y se devuelve al loop del teclado
             @pixel_actual
-	        d=m
+	        D=M
          	@16384
-	        d=d-a
+	        D=D-A
 	         @loop_teclado
-	         d;jle
+	         D;JLE
 	        
 	         @pixel_actual
-	        m=m-1  //decrece la posicion actual
+	        M=M-1  //decrece la posicion actual
 	        @fill_loop
-	     0;jmp
+	     0;JMP
