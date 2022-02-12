c258330ca0bdea6b6b97c9e8fb00d39fe82a7dbb
pablo <pablo.zulaica@upb.edu.co>
2022-02-11 22:02:09 -0500

    se creo la condicion que si se presiona f se pone la pantalla en negro

--- problem/program.asm
+++ problem/program.asm
@@ -2,5 +2,59 @@
 // 000416037
 // pablo.zulaica@upb.edu.co
 (START)
-    @START
-    0;JMP
+// declaracion de variables
+   
+	@24576// se crea la variable del input del teclado
+	d=a
+	@input_teclado
+	m=d
+	@70
+	d=a
+	@tecla_f // se crea la variable de la tecla f
+	
+    
+	(loop_teclado)
+
+			@24575
+	    d=a
+	    @pixel_actual
+	    m=d
+	    // se revisa si hay algun imput en el teclado y lo compara con el valor de la letra c
+	    @input_teclado
+	    a=m
+	    d=m
+	    @color_tinte
+	    m=-1
+	    @tecla_f
+    	d=d-m
+	    @fill_loop
+	    d;jeq
+	    // repite el loop hasta que detecte la tecla f 
+	    @loop_teclado
+        0;jmp
+
+		(fill_loop)		              
+	         @color_tinte  // se empieza el loop de el llenado de los pixeles de la pantalla si el valor de color_tinte es negro osea -1
+	         d=m
+	        @pixel_actual
+	         a=m   // los pixeles empiezan a cambiar su valor al color del tinte desde la posicion 24575 y se hace un loop de resta hasta la posicion 16384   
+	         m=d // que se traducen a la primera y ultima posicion de memoria en la pantalla, 
+            @pixel_actual
+	        d=m
+         	@16384
+	         d=m
+         	@16384
+	        d=d-a
+	         @fin
+	         d;jle
+	        
+	         @pixel_actual
+	        m=m-1  //decrece la posicion actual
+	        @fill_loop
+	     0;jmp
+
+
+          (fin)
+          @0
+          @fin
+	     0;jmp
